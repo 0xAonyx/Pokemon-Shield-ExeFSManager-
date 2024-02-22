6c21271da790d05826e984d32e51d4445f874f9d
Aonyx <slyskawaschool@Sams-MacBook-Pro.local>
2024-02-22 13:36:10 -0600

    Source code folder

--- source/main.cpp
+++ source/main.cpp
@@ -1,53 +1,87 @@
-#include <unistd.h>
-#include <string.h>
+//includes ascii art, check if enabled, color coded output, bool allowing input
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
 #include <switch.h>
+#include <sys/stat.h>
 
-int main(int argc, char **argv)
-{
-	
-    gfxInitDefault();
-	consoleInit(NULL);
-	printf("ExeFS Manager - SMO (By CVFD)\n");
+#include "otter.h"
+
+//check if file was properly renamed
+bool Rename(const char *src_path, const char *dest_path) {
+    if (rename(src_path, dest_path) != 0) {
+        printf("Failed to rename %s to %s.\n", src_path, dest_path);
+        return false;
+    }
+    return true;
+}
+
+bool fileExists(const char* filePath) {
+    struct stat buffer;
+    int exist = stat(filePath, &buffer);
+    if(exist == 0) {
+        return true; //enabled
+    } else {
+        return false; //disabled
+    }
+}
+
+int main(int argc, char **argv) {
+    consoleInit(NULL);
+    const char* state;
+    bool enabled = false;
+    printf("ExeFS Manager - Pokemon Sword (By Aonyx)\n");
+    //printf("Please reach out on discord (aonyx2) with any questions!\n")
+    //otter ascii
+    printf("\n");
+    printf("%s\n", otter);
+    printf("\n");
+    
+    //check if it is enabled
+    if(fileExists("/atmosphere/contents/01008DB008C2C000")) {
+            state = "ENABLED";
+            enabled = true;
+            printf("ExeFS are currently: \033[32m%s\033[0m\n", state);
+        } else if (fileExists("/atmosphere/contents/Shield_ExeFSMods_Disabled")) {
+            state = "DISABLED";
+            enabled = false;
+            printf("ExeFS are currently: \033[31m%s\033[0m\n", state);
+        }else{
+            state = "NOT FOUND";
+            printf("ExeFS are currently: \033[33m%s\033[0m\n", state);
+        }
+    
     printf("Press 'A' to Enable ExeFS Mods\n");
     printf("Press 'B' to Disable ExeFS Mods\n");
-	printf("Press '+' to Exit\n");
-    printf("This App will auto-exit when you choose an option.\n");
-	
-	while(appletMainLoop())
-    {
-        hidScanInput();
+    printf("Press '+' to Exit\n");
+    
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    PadState pad;
+    padInitializeDefault(&pad);
 
-        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-		if(kDown & KEY_B)
-		{
-        rename("/atmosphere/titles/0100000000010000/exefs/main", "/atmosphere/titles/0100000000010000/exefs/exefs.mod");
-            break;
-                        
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gfxWaitForVsync();
-		}
-		
-		if(kDown & KEY_A)
-		{
-        rename("/atmosphere/titles/0100000000010000/exefs/exefs.mod", "/atmosphere/titles/0100000000010000/exefs/main");
-            break;
-            
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gfxWaitForVsync();
-		}
-		
-        if (kDown & KEY_PLUS) break;
-		
-        gfxFlushBuffers();
-        gfxSwapBuffers();
-        gfxWaitForVsync();
+    while(appletMainLoop()) {
+        padUpdate(&pad);
+        u64 kDown = padGetButtonsDown(&pad);
+        if((kDown & HidNpadButton_A) && !enabled) {
+            if(!Rename("/atmosphere/contents/Shield_ExeFSMods_Disabled", "/atmosphere/contents/01008DB008C2C000")) {
+                printf("Error enabling ExeFS Mods.\n");
+            } else {
+                printf("ExeFS Mods Enabled.\n");
+                enabled = true;
+            }
+        }
+        if((kDown & HidNpadButton_B) && enabled) {
+            if(!Rename("/atmosphere/contents/01008DB008C2C000", "/atmosphere/contents/Shield_ExeFSMods_Disabled")) {
+                printf("Error disabling ExeFS Mods.\n");
+            } else {
+                printf("ExeFS Mods Disabled.\n");
+                enabled = false;
+            }
+        }
+        if(kDown & HidNpadButton_Plus) break;
+        consoleUpdate(NULL);
     }
 
-    consoleExit(NULL);		
-    gfxExit();
+    consoleExit(NULL);
     return 0;
 }
